4047afe53a71438d78ac95f273c421616e056379
Dorian Henry <dorian.henry2103@gmail.com>
2023-05-15 23:18:29 -0500

    Turning IN

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+// connect to db with mysql, use .env file to secure password (also added to .gitignore)
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "DJH2003djh",
+    database: "employee_db",
+  },
+  // confirm connection to db with yellow console log in terminal
+  console.log("Connected to the employee_db database ")
+);
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,13 @@
+const db = require("./config/connection.js");
+const selectTask = require("./lib/selectTask.js");
+
+// handle errors or start application
+db.connect(function (err) {
+  if (err) throw err;
+  init();
+});
+
+// start application, show title special-text and present task list prompt (using 'selectTask')
+function init() {
+  selectTask();
+}
